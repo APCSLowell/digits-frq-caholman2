73662913eb798067cebdc63e15ab217198a469c6
caholman2 <142835012+caholman2@users.noreply.github.com>
2024-04-15 11:28:37 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -18,7 +18,7 @@ public class Digits
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
 		for(int i=0; i<digitList.size(); i++){
-			if(digitList.get(i) => digitList.get(i+1))
+			if(digitList.get(i) >= digitList.get(i+1))
 				return false;
 		}
 		return true;
