374dd9517ab9aed80dee400ab63370bdc2adc7c7
caholman2 <142835012+caholman2@users.noreply.github.com>
2024-04-15 17:10:04 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,19 +6,21 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    int len = (num/10) +1;
+	{ /* to be implemented in part (a) */
 		digitList = new ArrayList<Integer>();
-		for(int i=0; i<len; i++){
-			digitList.add(0, num%10);
-			num = num/10;
+		if(num == 0)
+			digitList.add(0);
+		int n = num;
+		while(n > 0){
+			digitList.add(0, n%10);
+			n = n/10;
 		}
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
 		for(int i=0; i<digitList.size()-1; i++){
-			if(digitList.get(i) >= digitList.get(i+1)){
+			if(digitList.get(i+1) <= digitList.get(i)){
 				return false;
 			}
 		}
